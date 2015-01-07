f5fce825381dde7e2606260c33fa5fd8c002cf18
Kathy Wang <k.wang6898@gmail.com>
2015-01-07 13:06:07 -0500

    gameplans for every function we are afraid of writing

--- Sudoku.java
+++ Sudoku.java
@@ -25,12 +25,21 @@ public class Sudoku{
     }
 
     public void remove(){
-    }
+	//difficulty means that the probability that a number will be removed
+	//easy: 30% chance
+	//medium: 50% chance
+	//hard: 70% chance 
+	//for loop that goes through rows and columns. if there is something there, do the random check & remove & go on
+	//minimum 1 or 2 in each row/col/group box
+   }
 
     public void solver(){
     }
 
     public boolean checker(){
+	//for loop going through rows/cols/groups 
+	//inside is an arrayList {1,2,3,4,5,6,7,8,9} 
+	//remove as you go through, if false stop the whole thing
     }
 
     public void printSoln(){ 
