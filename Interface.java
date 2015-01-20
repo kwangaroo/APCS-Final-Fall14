5e1b41f01b1db72e9b89029ed8b07eb9b355168b
tzhang01 <tinazhang1197@gmail.com>
2015-01-19 20:39:11 -0500

    added board

--- Interface.java
+++ Interface.java
@@ -5,13 +5,15 @@ import javax.swing.border.*;
 import java.awt.event.*;
 
 public class Interface extends JFrame implements ActionListener{
+    private JTextField blank[][] = new JTextField[9][9];
+    private JPanel grid[][] = new JPanel[3][3];
 	private JButton check, easy, medium, hard, solution;
 
 	public Interface(){
 		setTitle("Sudoku");
-		//getContentPane().add(createCenterPanel(), BorderLayout.CENTER);
 		getContentPane().add(createTopPanel(), BorderLayout.NORTH);
-		setMinimumSize(new Dimension(600,300));
+		getContentPane().add(createCenterPanel(), BorderLayout.CENTER);
+		setMinimumSize(new Dimension(450,450));
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
@@ -47,14 +49,32 @@ public class Interface extends JFrame implements ActionListener{
 		return topPanel;
 	}
 
-//	public JPanel createCenterPanel(){
-	//}
+	public JPanel createCenterPanel(){
+		JPanel centerPanel = new JPanel(new GridLayout(3,3,5,5));
 
+		for(int i=0;i<9;i++){
+			for(int j=0;j<9;j++){
+		    	blank[i][j] = new JTextField(1);
+			}
+	    }
 	
-	public JLabel createLabel(String text){
-		JLabel label = new JLabel(text);
-		label.setHorizontalAlignment(JLabel.CENTER);
-		return label;
+	    for(int i=0;i<3;i++){
+			for(int j=0;j<3;j++){
+			    grid[i][j] = new JPanel(new GridLayout(3,3));
+			}
+	    }
+
+	    for(int i=0; i<3; i++){
+			for(int j=0;j<3;j++){
+			    for(int x=0;x<3;x++){
+					for(int y =0; y<3;y++){
+					    grid[i][j].add(blank[y+i*3][x+j*3]);
+					}
+			    }
+			    centerPanel.add(grid[i][j]);
+			}
+	    }
+		return centerPanel;
 	}
 
     public void actionPerformed(ActionEvent e){
